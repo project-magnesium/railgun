4129e8cfe66f1647c030cdbf67fe2054dd7fdb03
Lucas Gismondi <lucas@gismondigroup.com>
2023-04-23 16:31:36 -0400

    Fixes mobile login

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,78 +1,142 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CookieManager from '@react-native-cookies/cookies';
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { Button } from 'ui';
+import { SafeAreaView, Platform } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
+import * as AuthSession from 'expo-auth-session';
 import * as Google from 'expo-auth-session/providers/google';
-import User from 'common/objects/User';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import * as SecureStore from 'expo-secure-store';
+
+import { trpcConfig, UserInterface } from 'common';
+import { Button, Provider, Text, theme } from 'ui';
 import { VITE_GOOGLE_IOS_CLIENT_ID, VITE_GOOGLE_ANDROID_CLIENT_ID, VITE_DOMAIN_NAME, VITE_API_BASE_URL } from '@env';
 
 WebBrowser.maybeCompleteAuthSession();
 
-export default function App() {
-    const androidClientId = VITE_GOOGLE_ANDROID_CLIENT_ID;
-    const iosClientId = VITE_GOOGLE_IOS_CLIENT_ID;
+const Stack = createNativeStackNavigator();
+const androidClientId = VITE_GOOGLE_ANDROID_CLIENT_ID;
+const iosClientId = VITE_GOOGLE_IOS_CLIENT_ID;
+
+const setCookie = async (idToken: string) => {
+    try {
+        const newCookie = {
+            httpOnly: true,
+            domain: `.${VITE_DOMAIN_NAME}`,
+            secure: true,
+            name: 'cred',
+            value: idToken,
+            url: VITE_API_BASE_URL,
+        };
+
+        await CookieManager.set(`https://www.${VITE_DOMAIN_NAME}`, newCookie);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const handleSetCredentials = async (idToken: string | undefined, refreshToken: string | undefined) => {
+    if (idToken) await setCookie(idToken);
+    if (refreshToken) await SecureStore.setItemAsync('refreshToken', refreshToken);
+};
+
+const Login = () => {
     const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
         androidClientId,
         iosClientId,
         responseType: 'id_token',
     });
-    const user = new User();
+
+    const handleLoginSuccess = async (idToken: string, refreshToken: string | undefined) => {
+        await handleSetCredentials(idToken, refreshToken);
+
+        const userInterface = new UserInterface();
+        await userInterface.getOrCreateProfile();
+    };
 
     useEffect(() => {
         if (response?.type === 'success' && response.authentication && response.authentication.idToken) {
-            setCookie(response.authentication.idToken);
+            handleLoginSuccess(response.authentication.idToken, response.authentication.refreshToken);
         }
     }, [request, response]);
 
-    const setCookie = async (idToken: string) => {
-        try {
-            const newCookie = {
-                httpOnly: true,
-                domain: `.${VITE_DOMAIN_NAME}`,
-                secure: true,
-                name: 'cred',
-                value: idToken,
-                url: VITE_API_BASE_URL,
-            };
-
-            await CookieManager.set(`https://www.${VITE_DOMAIN_NAME}`, newCookie);
-        } catch (e) {
-            console.log(e);
-        }
-    };
-
-    const handleTest = async () => {
-        await user.test();
-    };
-
     return (
-        <View style={styles.container}>
+        <>
             <Button
-                disabled={!request}
+                isDisabled={!request}
                 onPress={() => {
                     promptAsync();
                 }}
-            >
-                <Text>Sign in with Google</Text>
-            </Button>
-            <Button disabled={!request} onPress={handleTest}>
-                <Text>Set Date</Text>
-            </Button>
-        </View>
+                title="Sign in with Google"
+            />
+        </>
+    );
+};
+
+const StackNavigator = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+    const navigation = useNavigation<NativeStackNavigationProp<any>>();
+
+    useEffect(() => {
+        trpcConfig.set({
+            baseURL: VITE_API_BASE_URL,
+            handleLogin: async () => {
+                try {
+                    const refreshToken = (await SecureStore.getItemAsync('refreshToken')) ?? undefined;
+                    const refreshResponse = await AuthSession.refreshAsync(
+                        { refreshToken: refreshToken, clientId: Platform.OS === 'ios' ? iosClientId : androidClientId },
+                        Google.discovery
+                    );
+                    await handleSetCredentials(refreshResponse.idToken, undefined);
+
+                    return true;
+                } catch (e) {
+                    console.log(e);
+                }
+
+                navigation.navigate('login');
+                return false;
+            },
+        });
+    }, []);
+
+    const handleCheckLoggedIn = async () => {
+        const refreshToken = await SecureStore.getItemAsync('refreshToken');
+        if (refreshToken) setIsLoggedIn(true);
+    };
+
+    useEffect(() => {
+        handleCheckLoggedIn();
+    }, []);
+
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            {isLoggedIn ? (
+                <Stack.Screen name="welcome">
+                    {() => (
+                        <Provider>
+                            <Text>Welcome</Text>
+                        </Provider>
+                    )}
+                </Stack.Screen>
+            ) : (
+                <Stack.Screen name="login" component={Login} />
+            )}
+        </Stack.Navigator>
+    );
+};
+
+export default function App() {
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.palette.background.default }}>
+            <NavigationContainer theme={{ colors: { background: theme.palette.background.default } }}>
+                <StackNavigator />
+            </NavigationContainer>
+        </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-});
--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,15 +1,4 @@
 import { registerRootComponent } from 'expo';
-import { trpcConfig } from 'common';
 import App from './App';
-import { VITE_API_BASE_URL } from '@env';
-
-trpcConfig.set({
-    baseURL: VITE_API_BASE_URL,
-    handleLogin: () => {
-        // TODO: Implement how you want to handle mobile re authentication flow
-
-        return Promise.resolve(false);
-    },
-});
 
 registerRootComponent(App);
